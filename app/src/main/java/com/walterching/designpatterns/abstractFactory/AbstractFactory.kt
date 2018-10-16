2127e25a1d9e64cb4be46d6713e8b9bca06c6f25
Walter Ching <walter.ching.b@gmail.com>
2018-10-16 03:10:57 +0200

    Add comments and missing tests to abstract factory

--- app/src/main/java/com/walterching/designpatterns/abstractFactory/AbstractFactory.kt
+++ app/src/main/java/com/walterching/designpatterns/abstractFactory/AbstractFactory.kt
@@ -6,6 +6,10 @@ import com.walterching.designpatterns.abstractFactory.colors.IColor
 import com.walterching.designpatterns.factory.shapes.IShape
 
 abstract class AbstractFactory {
+
+    // This abstract factory doesn't really make much sense since this two
+    // items don't have nothing in common and it will require to implement
+    // nullable methods
     abstract fun getShape(type: ShapeType): IShape?
     abstract fun getColor(type: ColorType): IColor?
 }
--- app/src/test/java/com/walterching/designpatterns/abstractfactory/FactoryProducerUnitTest.kt
+++ app/src/test/java/com/walterching/designpatterns/abstractfactory/FactoryProducerUnitTest.kt
@@ -3,6 +3,8 @@ package com.walterching.designpatterns.abstractfactory
 import com.walterching.designpatterns.abstractFactory.ColorFactory
 import com.walterching.designpatterns.abstractFactory.FactoryProducer
 import com.walterching.designpatterns.abstractFactory.ShapeFactory
+import com.walterching.designpatterns.abstractFactory.types.ColorType
+import com.walterching.designpatterns.abstractFactory.types.ShapeType
 import org.junit.Before
 import org.junit.Test
 
@@ -22,14 +24,36 @@ class FactoryProducerUnitTest {
     }
 
     @Test
-    fun colorFactoryIsReturnedByFactory() {
+    fun colorRedIsReturnedByFactory() {
         val colorFactory = factoryProducer.getFactory(FactoryProducer.FactoryType.Color)
-        assert(colorFactory is ColorFactory)
+        val red = colorFactory.getColor(ColorType.Red)
+        assert(red?.fill() == "Filling red...")
+    }
+
+    @Test
+    fun colorBlueIsReturnedByFactory() {
+        val colorFactory = factoryProducer.getFactory(FactoryProducer.FactoryType.Color)
+        val blue = colorFactory.getColor(ColorType.Blue)
+        assert(blue?.fill() == "Filling blue...")
     }
 
     @Test
     fun shapeFactoryIsReturnedByFactory() {
         val shapeFactory = factoryProducer.getFactory(FactoryProducer.FactoryType.Shape)
         assert(shapeFactory is ShapeFactory)
+    }
+
+    @Test
+    fun diamondShapeIsReturnedByFactory() {
+        val shapeFactory = factoryProducer.getFactory(FactoryProducer.FactoryType.Shape)
+        val diamond = shapeFactory.getShape(ShapeType.Diamond)
+        assert(diamond?.draw() == "Draw Diamond")
+    }
+
+    @Test
+    fun triangleShapeIsReturnedByFactory() {
+        val shapeFactory = factoryProducer.getFactory(FactoryProducer.FactoryType.Shape)
+        val triangle = shapeFactory.getShape(ShapeType.Triangle)
+        assert(triangle?.draw() == "Draw Triangle")
     }
 }
